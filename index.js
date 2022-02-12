2db7e7999616d0ceb890245b6d386c72b9af68ac
Khal3dAMARI <khaled.amari@novhadrim.com>
2022-02-12 23:36:37 +0100

    Rework using youtube API

--- index.js
+++ index.js
@@ -1,30 +1,68 @@
-window.addEventListener('load', () => {
-  document.getElementById('signout').style.display = "none";
-  document.getElementById('title').innerText = "Sign in";
-})
+const CLIENT_ID = "633090595912-dl7b4kusehelmr5nuspeqt3mb71f7kl5.apps.googleusercontent.com";
+const DISCOVERY_DOCS = [
+  'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'
+];
+const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
 
+const authorizeButton = document.getElementById('authorize-button');
+const signoutButton = document.getElementById('signout-button');
+const content = document.getElementById('content');
+const channelForm = document.getElementById('channel-form');
+const channelInput = document.getElementById('channel-input');
+const videoContainer = document.getElementById('video-container');
 
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  // document.getElementsByClassName('g-signin2').style.display = "none";
-  // document.getElementById('signout').style.display = "block";
-  document.getElementById("name").innerText = profile.getName();
-  // document.getElementById('signout').addEventListener("click", signOut);
-  document.getElementById('title').innerText = `Hello ${profile.getName()}`;
-  // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  // console.log('Name: ' + profile.getName());
-  // console.log('Image URL: ' + profile.getImageUrl());
-  // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+const defaultChannel = 'القرآن الكريم';
 
+// Load auth2 library
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
 };
 
-function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-    console.log('User signed out.');
-    alert('User signed out.');
-  });
-  // document.getElementById('signout').style.display = "none";
-  // document.getElementById('signout').removeEventListener("click", signOut);
-  // document.getElementsByClassName('g-signin2').style.display = "block";
+// Init API client library and set up sign in listeners
+function initClient() {
+  gapi.client
+    .init({
+      discoveryDocs: DISCOVERY_DOCS,
+      clientId: CLIENT_ID,
+      scope: SCOPES
+    })
+    .then(() => {
+      // Listen for sign in state changes
+      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+      // Handle initial sign in state
+      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+      authorizeButton.onclick = handleAuthClick;
+      signoutButton.onclick = handleSignoutClick;
+    });
+};
+
+// Update UI sign in state changes
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    authorizeButton.style.display = 'none';
+    signoutButton.style.display = 'block';
+    content.style.display = 'block';
+    videoContainer.style.display = 'block';
+    getChannel(defaultChannel);
+  } else {
+    authorizeButton.style.display = 'block';
+    signoutButton.style.display = 'none';
+    content.style.display = 'none';
+    videoContainer.style.display = 'none';
+  }
+};
+
+// Handle login
+function handleAuthClick() {
+  gapi.auth2.getAuthInstance().signIn();
+};
+
+// Handle logout
+function handleSignoutClick() {
+  gapi.auth2.getAuthInstance().signOut();
+};
+
+// Get channel from API
+function getChannel(channel) {
+  console.log(channel);
 }
