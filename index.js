b0fea17d21fec89bd3fbb79ef9559eb926904e5c
Khal3dAMARI <khaled.amari@novhadrim.com>
2022-02-12 19:07:07 +0100

    Add google SignIn / signUp

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello heroku");
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  document.getElementsByClassName('g-signin2').style.display = "none";
+  document.getElementById('signout').style.display = "block";
+  document.getElementById("name").innerText = profile.getName();
+  document.getElementById('signout').addEventListener("click", signOut);
+  // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  // console.log('Name: ' + profile.getName());
+  // console.log('Image URL: ' + profile.getImageUrl());
+  // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+};
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+    alert('User signed out.');
+  });
+  document.getElementById('signout').style.display = "none";
+  document.getElementById('signout').removeEventListener("click", signOut);
+  document.getElementsByClassName('g-signin2').style.display = "block";
+}
